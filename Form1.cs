ed07539992c6e807b075606b4da7ab281ac08f5c
Rashyd <rashydramdwn@gmail.com>
2026-09-12 11:56:15 +0300

    Programming the (Show Result) button

--- Form1.cs
+++ Form1.cs
@@ -137,5 +137,10 @@ namespace CalculateorProject
         {
             Operation = "×";
         }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            CalculatingAndDisplayingTheResult();
+        }
     }
 }
