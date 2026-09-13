c77a6ffba334195cb04c03989ecf2bef14a5fce5
Rashyd <rashydramdwn@gmail.com>
2026-09-13 10:35:09 +0300

    Bringing the Convert button to life and making it functioonal

--- frmBinaryToDecimal.cs
+++ frmBinaryToDecimal.cs
@@ -16,5 +16,29 @@ namespace CalculateorProject
         {
             InitializeComponent();
         }
+        private void ConvertBinaryToDecimal()
+        {
+            string BinaryStr;
+            BinaryStr = txtBinaryInput.Text;
+
+            long DecimalNum = 0;
+            long Base = 1;
+
+            for(int i = BinaryStr.Length - 1; i >= 0; i--)
+            {
+                if(BinaryStr[i] == '1')
+                {
+                    DecimalNum += Base;
+                }
+
+                Base *= 2;
+            }
+
+            lblResultValue.Text = Convert.ToString(DecimalNum);
+        }
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            ConvertBinaryToDecimal();
+        }
     }
 }
